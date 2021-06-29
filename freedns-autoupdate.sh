a0c62eaeb3e725cf8c32853e8a09620071f57524
Ligh7n1ng <[alexey892@outlook.com]>
2021-06-29 21:12:28 +0300

    systemd units added, some changes in script

--- freedns-autoupdate.sh
+++ freedns-autoupdate.sh
@@ -17,20 +17,31 @@ then
     exit 1
 fi
 
-if [ $(echo "ls $LogFile | grep '^$LogFile' | wc -l" | bash) = 1 ]
+test $LogFile
+
+if [ $(echo $?) = 0 ]
 then
     echo -e "Using "$LogFile" log file."
 else
-    LogFile="/var/log/freedns-autoupdate/ip.log"
-    echo "Log file is not specified or exist. Creating it in default path $LogFile"
-    touch "$LogFile"
-    PermErrorCheck
+    LogFile="/var/log/freedns-autoupdate/ddns-ip.log"
+    echo "Log file is not specified. Creating it in the default path $LogFile"
+    
+    test -f $LogFile
+    
+    if [ $(echo $?) -ne 0 ]
+    then
+    	echo "Test record" > $LogFile
+	PermErrorCheck
+    else 
+	touch "$LogFile"
+	PermErrorCheck
+    fi
 fi
 
 LastIP=$(head -n 1 "$LogFile")
 CurrentIP=$(curl -s https://api.ipify.org/)
 
-if [ $LastIP = $CurrentIP ]
+if [ "$LastIP" = "$CurrentIP" ]
 then
     echo "IP address has not been changed. No need to update."
 else
